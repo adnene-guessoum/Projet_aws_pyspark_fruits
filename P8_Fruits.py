196b4150241589263f54166e2c0dea8b40080609
Adnene_wsl <adnen.guessoum@gmail.com>
2022-08-17 17:52:12 +0200

    ajout des imports et pip install pandas et numpy dans virtual env

--- P8_Fruits.py
+++ P8_Fruits.py
@@ -1,3 +1,28 @@
+# général
 import pandas as pd
 import numpy as np
+import os
+import time
+
+#images
+from PIL import image
+from IPython.display import Image
+from PIL import Image as Image_PIL
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+#import pyspark
+from pyspark import SparkContext, SparkConf
+from pyspark.sql import SparkSession
+from pyspark.ml.image import ImageSchema
+#import sparkdl
+from sparkdl import DeepImageFeaturizer
+
+Image_path = "..\S3_test_P8"
+
+list_img = [file for file in os.listdir(Image_path)]
+print(len(list_img))
+
+
+
 
