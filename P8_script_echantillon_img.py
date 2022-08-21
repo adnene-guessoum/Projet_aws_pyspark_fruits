78c4c06aa12e12778b28b83996cb5a64ce03c15f
Adnene_wsl <adnen.guessoum@gmail.com>
2022-08-21 17:08:41 +0200

    script de selection d'images et de chargement dans un bucket aws s3 fonctionne.

--- P8_script_echantillon_img.py
+++ P8_script_echantillon_img.py
@@ -1,11 +1,13 @@
 # général
-import pandas as pd
-import numpy as np
 import random
 import os
+import sys
 import shutil
 
-Image_path = "Dev/Projet_8/archive/fruits-360_dataset/fruits-360/Training"
+import boto3
+
+
+Image_path = "Dev/Projet-8/archive/fruits-360_dataset/fruits-360/Training"
 
 def random_image_selection(num_img, path):
     os.chdir(path)
@@ -29,7 +31,7 @@ def random_image_selection(num_img, path):
         labels_img_lst.append(labels_img_folder)
     #placer les images sélectionnées dans un nouveau dossier
     dossier_source = path
-    dossier_arrivee = r"/home/adnene/Dev/Projet_8/echantillon-img"
+    dossier_arrivee = r"/home/adnene/Dev/Projet-8/echantillon-img"
 
     for i,j in zip(reduced_data, labels_img_lst):
         source = i
@@ -38,9 +40,70 @@ def random_image_selection(num_img, path):
         shutil.copy(source, destination)
     print('Done')
 
-def random_selection_images_files(num_img, num_files, pth):
+def targetted_random_image_selection(num_img, num_files, pth, to_S3):
+    os.chdir(pth)
+    thisdir = os.getcwd()
+    list_subdir = [f for f in os.listdir(thisdir) if os.path.isdir(os.path.join(thisdir, f))]
+
+    chosen_fruits = random.sample(list_subdir, num_files)
+
+    data = []
+    for i in chosen_fruits:
+        path_to_jpg = os.path.join(thisdir,i)
+        onlyfiles = [f for f in os.listdir(path_to_jpg) if os.path.isfile(os.path.join(path_to_jpg, f))]
+        chosen_image = random.sample(onlyfiles, num_img)
+        data.append(chosen_image)
+
+    print(data)
+    print("---------------------------------------------------")
+    print(chosen_fruits)
+
     
-    pass
+    #placer les images sélectionnées dans un nouveau dossier
+    dossier_source = r"/home/adnene"
+    dossier_arrivee = r"/home/adnene/Dev/Projet-8/echantillon-img"
+
+    for i,j in zip(chosen_fruits, data):
+        for z in j:
+            source = os.path.join(dossier_source, pth, i, z)
+            destination = os.path.join(dossier_arrivee, i, z)
+            os.makedirs(os.path.dirname(destination), exist_ok=True)
+            shutil.copy(source, destination)
+    print('Done')
+
+    if to_S3 == True :
+        os.chdir(dossier_arrivee)
+        thisdir = os.getcwd()
+
+        AWS_REGION = "eu-west-1"
+        client = boto3.client("s3", region_name=AWS_REGION)
+
+        #bucket creation :
+        bucket = 'echantillon-img'
+        location = {'LocationConstraint': AWS_REGION}
+        response = client.create_bucket(Bucket=bucket, CreateBucketConfiguration=location)
+
+        print("Amazon S3 bucket has been created")
+
+
+        destination = 'echantillon-img'
+
+        for r, d, f in os.walk(thisdir):
+            for file in f :
+                path = os.path.join(r,file)
+                r_path = os.path.relpath(path, thisdir)
+                s3_path = r_path
+
+                try:
+                    client.head_object(Bucket=bucket, Key=s3_path)
+                    print("le path existe déja sur s3. abandon %s..." % s3_path)
+
+                except:
+                    print('CHARGEMENT : %s ...' % s3_path)
+                    client.upload_file(path, bucket, s3_path)
+
+        print("Done for S3")
 
 if __name__ == '__main__':
-    random_image_selection(300, Image_path)
+    #random_image_selection(300, Image_path)
+    targetted_random_image_selection(3, 4, Image_path, to_S3=True)
